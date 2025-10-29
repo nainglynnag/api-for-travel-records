542817b452f7ac6ca7ec8921201a1e0233a11952
NaingLynnAung <nainglynnaung.lynn@gmail.com>
2025-10-29 23:52:36 +0700

    Allow cors for all routes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+// To allow cors
+// Method 1
+
+// app.use((req, res, next) => {
+//   res.append("Access-Control-Allow-Origin", "*");
+//   res.append("Access-Control-Allow-Methods", "*");
+//   res.append("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+
+// Method 2 - using cors
+const cors = require("cors");
+app.use(cors());
+
+// cors - customization
+// app.use(cors({
+//   origin: ["http://abc.com","http://abc.com",],
+//   methods: ["GET","POST"],
+//   allowedHeaders: ["Authorization","Content-Type"]
+// }));
+
 // For query string parsing
 app.set('query parser', 'extended');
 
